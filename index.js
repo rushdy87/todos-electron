0e05f04ae1d1f42a7a0f90a53c41c0c61370e165
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-01-28 02:02:29 +0300

    Creating Separate Windows

--- index.js
+++ index.js
@@ -3,22 +3,38 @@ const electron = require('electron');
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     webPreferences: { nodeIntegration: true, contextIsolation: false },
   });
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+  mainWindow.on('closed', () => app.quit());
 
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
 });
 
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add new ToDo',
+  });
+  addWindow.loadURL(`file://${__dirname}/add.html`);
+}
+
 const menuTemplate = [
   {
     label: 'File',
     submenu: [
-      { label: 'New Todo' },
+      {
+        label: 'New Todo',
+        click() {
+          createAddWindow();
+        },
+      },
       {
         label: 'Quit',
         accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
