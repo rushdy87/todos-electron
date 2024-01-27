82a9171952b8dbb7aa1d3b653c70d02581da8896
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-01-28 01:06:34 +0300

    Constructing Menu Bars

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
@@ -9,4 +9,14 @@ app.on('ready', () => {
     webPreferences: { nodeIntegration: true, contextIsolation: false },
   });
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
+
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [{ label: 'New Todo' }],
+  },
+];
